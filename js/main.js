c1b88c729a83e28253315958b4786bcb8efad246
Elena <shtanko1991@gmail.com>
2021-04-07 23:21:35 +0300

    mousedown-mousup

--- js/main.js
+++ js/main.js
@@ -22,27 +22,72 @@ signUpBtn.addEventListener('click', () => {
 console.log(passInputUp.getAttribute('type') == 'password');
 
 
-eyeUp.addEventListener('click', function () {
-    if (passInputUp.getAttribute('type') == 'password') {
-        passInputUp.setAttribute('type','text')
-        eyeUp.classList.remove('fa-eye-slash')
-        eyeUp.classList.add('fa-eye')
-    } else {
+// eyeUp.addEventListener('click', function () {
+//     if (passInputUp.getAttribute('type') == 'password') {
+//         showUpPass()
+//     } else {
+//         HideUpPass()
+//     }
+   
+// })
+// eyeIn.addEventListener('click', function () {
+//     if (passInputIn.getAttribute('type') == 'password') {
+//         showInPass()
+//     } else {
+//         HideInPass()
+//     }
+   
+// })
+
+
+function showInPass() {
+    passInputIn.setAttribute('type','text')
+    eyeIn.classList.remove('fa-eye-slash')
+    eyeIn.classList.add('fa-eye')
+}
+function HideInPass() {
+    passInputIn.setAttribute('type','password')
+        eyeIn.classList.remove('fa-eye')
+        eyeIn.classList.add('fa-eye-slash')
+}
+function showUpPass() {
+    passInputUp.setAttribute('type','text')
+    eyeUp.classList.remove('fa-eye-slash')
+    eyeUp.classList.add('fa-eye')
+}
+function HideUpPass() {
         passInputUp.setAttribute('type','password')
         eyeUp.classList.remove('fa-eye')
         eyeUp.classList.add('fa-eye-slash')
-    }
+}
+
+
+
+
+
+
+eyeUp.addEventListener('mousedown', () => {
+   
+        showUpPass()
+        console.log('mousedown')
    
 })
-eyeIn.addEventListener('click', function () {
-    if (passInputIn.getAttribute('type') == 'password') {
-        passInputIn.setAttribute('type','text')
-        eyeIn.classList.remove('fa-eye-slash')
-        eyeIn.classList.add('fa-eye')
-    } else {
-        passInputIn.setAttribute('type','password')
-        eyeIn.classList.remove('fa-eye')
-        eyeIn.classList.add('fa-eye-slash')
-    }
+
+eyeUp.addEventListener('mouseup', ()=> {
+    console.log('mouseup')
+    HideUpPass()
+
+})
+eyeIn.addEventListener('mousedown', () => {
+    showInPass()
+        
+        console.log('mousedown');
    
 })
+
+eyeIn.addEventListener('mouseup', ()=> {
+    console.log('mouseup');
+    HideInPass()
+
+})
+
